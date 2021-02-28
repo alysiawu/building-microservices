e2ea1fa6b67426f307992db0bcc085e7b6f3d4b0
alysiawu <alysiawu886@gmail.com>
2021-02-27 21:26:04 -0800

    init

---
+++ working/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/nicholasjackson/working/handlers"
+)
+
+func main() {
+	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+	// 	log.Println("Hello World")
+	// 	d, err := ioutil.ReadAll(r.Body)
+	// 	// log.Printf("Data %s\n", d)
+	// 	if err != nil {
+	// 		http.Error(rw, "Oops", http.StatusBadRequest)
+	// 		// rw.WriteHeader(http.StatusBadRequest)
+	// 		// rw.Write([]byte("oops"))
+	// 		return
+	// 	}
+
+	// 	fmt.Fprintf(rw, "Hello %s", d)
+
+	// })
+
+	// http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+	// 	log.Println("Goodby World")
+	// })
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	http.ListenAndServe(":9092", nil)
+}
